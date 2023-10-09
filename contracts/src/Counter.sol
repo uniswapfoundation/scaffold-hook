a4e73b6835c7001483b45c7cacf85a0446cdd029
saucepoint <saucepoint@protonmail.com>
2023-10-09 14:48:49 -0400

    counter hooks

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,16 +1,83 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract Counter {
-    uint256 public number = 0;
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
+import {BaseHook} from "v4-periphery/BaseHook.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+
+contract Counter is BaseHook {
+    using PoolIdLibrary for PoolKey;
+
+    // NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+
+    mapping(PoolId => uint256 count) public beforeSwapCount;
+    mapping(PoolId => uint256 count) public afterSwapCount;
+
+    mapping(PoolId => uint256 count) public beforeModifyPositionCount;
+    mapping(PoolId => uint256 count) public afterModifyPositionCount;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return Hooks.Calls({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeModifyPosition: true,
+            afterModifyPosition: true,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false
+        });
     }
 
-    function increment() public {
-        number++;
+    // -----------------------------------------------
+    // NOTE: see IHooks.sol for function documentation
+    // -----------------------------------------------
+
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+        external
+        override
+        returns (bytes4)
+    {
+        beforeSwapCount[key.toId()]++;
+        return BaseHook.beforeSwap.selector;
+    }
+
+    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+        external
+        override
+        returns (bytes4)
+    {
+        afterSwapCount[key.toId()]++;
+        return BaseHook.afterSwap.selector;
+    }
+
+    function beforeModifyPosition(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata,
+        bytes calldata
+    ) external override returns (bytes4) {
+        beforeModifyPositionCount[key.toId()]++;
+        return BaseHook.beforeModifyPosition.selector;
+    }
+
+    function afterModifyPosition(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyPositionParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4) {
+        afterModifyPositionCount[key.toId()]++;
+        return BaseHook.afterModifyPosition.selector;
     }
 }
--- contracts/test/Counter.t.sol
+++ contracts/test/Counter.t.sol
@@ -1,24 +1,71 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {TickMath} from "@uniswap/v4-core/contracts/libraries/TickMath.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {Deployers} from "@uniswap/v4-core/test/foundry-tests/utils/Deployers.sol";
+import {CurrencyLibrary, Currency} from "@uniswap/v4-core/contracts/types/Currency.sol";
+import {HookTest} from "./utils/HookTest.sol";
+import {Counter} from "../src/Counter.sol";
+import {HookMiner} from "./utils/HookMiner.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract CounterTest is HookTest, Deployers {
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+
+    Counter counter;
+    PoolKey poolKey;
+    PoolId poolId;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        // creates the pool manager, test tokens, and other utility routers
+        HookTest.initHookTestEnv();
+
+        // Deploy the hook to an address with the correct flags
+        uint160 flags = uint160(
+            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_MODIFY_POSITION_FLAG
+                | Hooks.AFTER_MODIFY_POSITION_FLAG
+        );
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(address(this), flags, 0, type(Counter).creationCode, abi.encode(address(manager)));
+        counter = new Counter{salt: salt}(IPoolManager(address(manager)));
+        require(address(counter) == hookAddress, "CounterTest: hook address mismatch");
+
+        // Create the pool
+        poolKey = PoolKey(Currency.wrap(address(token0)), Currency.wrap(address(token1)), 3000, 60, IHooks(counter));
+        poolId = poolKey.toId();
+        manager.initialize(poolKey, SQRT_RATIO_1_1, ZERO_BYTES);
+
+        // Provide liquidity to the pool
+        modifyPositionRouter.modifyPosition(poolKey, IPoolManager.ModifyPositionParams(-60, 60, 10 ether), ZERO_BYTES);
+        modifyPositionRouter.modifyPosition(poolKey, IPoolManager.ModifyPositionParams(-120, 120, 10 ether), ZERO_BYTES);
+        modifyPositionRouter.modifyPosition(
+            poolKey,
+            IPoolManager.ModifyPositionParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 10 ether),
+            ZERO_BYTES
+        );
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    function testCounterHooks() public {
+        // positions were created in setup()
+        assertEq(counter.beforeModifyPositionCount(poolId), 3);
+        assertEq(counter.afterModifyPositionCount(poolId), 3);
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        assertEq(counter.beforeSwapCount(poolId), 0);
+        assertEq(counter.afterSwapCount(poolId), 0);
+
+        // Perform a test swap //
+        int256 amount = 100;
+        bool zeroForOne = true;
+        swap(poolKey, amount, zeroForOne, ZERO_BYTES);
+        // ------------------- //
+
+        assertEq(counter.beforeSwapCount(poolId), 1);
+        assertEq(counter.afterSwapCount(poolId), 1);
     }
 }
