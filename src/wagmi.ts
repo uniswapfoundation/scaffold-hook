4a0890b03fc7cad95460522a5a5f3546c735ed55
saucepoint <saucepoint@protonmail.com>
2023-10-09 14:14:46 -0400

    wagmi init

---
+++ src/wagmi.ts
@@ -0,0 +1,40 @@
+import { configureChains, createConfig } from 'wagmi'
+import { foundry, goerli, mainnet } from 'wagmi/chains'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    ...(process.env.NODE_ENV === 'development' ? [goerli, foundry] : []),
+  ],
+  [
+    publicProvider(),
+  ],
+)
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
