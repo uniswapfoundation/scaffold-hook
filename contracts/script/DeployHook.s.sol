0f50c0eb5cef7f4d1b026ddb28d00a56e554c730
saucepoint <saucepoint@protonmail.com>
2023-10-12 12:39:41 -0400

    cleanup

--- contracts/script/DeployHook.s.sol
+++ contracts/script/DeployHook.s.sol
@@ -3,21 +3,16 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
 import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
-import {PoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
-import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
-import {PoolModifyPositionTest} from "@uniswap/v4-core/contracts/test/PoolModifyPositionTest.sol";
-import {PoolSwapTest} from "@uniswap/v4-core/contracts/test/PoolSwapTest.sol";
-import {PoolDonateTest} from "@uniswap/v4-core/contracts/test/PoolDonateTest.sol";
 import {HookMiner} from "../test/utils/HookMiner.sol";
 
 contract DeployHookScript is Script {
     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
-    PoolManager manager = PoolManager(payable(vm.envAddress("POOL_MANAGER_ADDR")));
+    address manager = payable(vm.envAddress("POOL_MANAGER_ADDR"));
 
     function setUp() public {}
 
     function run() public {
-        bytes memory constructorArgs = abi.encode(address(manager));
+        bytes memory constructorArgs = abi.encode(manager);
 
         // hook contracts must have specific flags encoded in the address
         uint160 flags = uint160(
@@ -41,16 +36,5 @@ contract DeployHookScript is Script {
 
         // verify proper create2 usage
         require(deployedHook == hookAddress, "DeployScript: hook address mismatch");
-
-        // Additional helpers for interacting with the pool
-        // deployRouters();
-    }
-
-    function deployRouters() internal {
-        vm.startBroadcast();
-        new PoolModifyPositionTest(IPoolManager(address(manager)));
-        new PoolSwapTest(IPoolManager(address(manager)));
-        new PoolDonateTest(IPoolManager(address(manager)));
-        vm.stopBroadcast();
     }
 }
