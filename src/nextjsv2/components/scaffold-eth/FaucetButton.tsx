026f8d2030d54bb552b4c642a9bae5964566ce38
saucepoint <saucepoint@protonmail.com>
2023-11-12 17:26:44 -0500

    fix hydration by removing spinner

--- src/nextjsv2/components/scaffold-eth/FaucetButton.tsx
+++ src/nextjsv2/components/scaffold-eth/FaucetButton.tsx
@@ -90,28 +90,7 @@ export const FaucetButton = () => {
   return (
     <Dropdown>
       <DropdownTrigger>
-        <Button
-          variant="ghost"
-          color="warning"
-          spinner={
-            <svg
-              className="animate-spin h-5 w-5 text-current"
-              fill="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-              <path
-                className="opacity-75"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                fill="currentColor"
-              />
-            </svg>
-          }
-          // spinnerPlacement="start"
-          isLoading={token0.isLoading || token1.isLoading || loading}
-          disabled={loading}
-        >
+        <Button variant="ghost" color="warning" disabled={token0.isLoading || token1.isLoading || loading}>
           <BanknotesIcon className="h-4 w-4" />
           <span className="ml-2">Faucet</span>
         </Button>
